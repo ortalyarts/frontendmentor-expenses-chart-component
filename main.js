6aaf7316fb9943e88d8ea8aa9f881aa85c044905
ortalyarts <148264882+ortalyarts@users.noreply.github.com>
2024-02-19 19:37:01 +0100

    whole project

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
+
 
  //include the   'async':false   parameter or the object data won't get captured when loading
-var jsonRawFile = $.getJSON({'url': "https://raw.githubusercontent.com/ortalyarts/frontendmentor-results-summary-component/main/data.json", 'async': false});
-console.log(jsonRawFile);
+var jsonRawFile = $.getJSON({'url': "https://raw.githubusercontent.com/ortalyarts/frontendmentor-expenses-chart-component/main/data.json", 'async': false});
+
 var jsonFile = jsonRawFile.responseJSON;
 
-console.log(jsonFile);
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-//NEW function
+
+
 function listItemsForEach() {
-    jsonFile.forEach((item) => {
+    jsonFile.forEach((item, i) => {
+        let height = item.amount / 5.584;
         el = document.createElement('div');
         el.innerHTML = 
-            `
-            
-            <div class="bar orange"></div>
+            `            
+            <div class="bar" ></div>
             <p>${item.day}</p>
             </div>
             `;
-        el.classList.add('day');    
+        el.classList.add('day');
+        el.setAttribute('id',`day-${i+1}`);
         document.querySelector('#bars-container').appendChild(el);
-        console.log(item); // prints each object (category) in the console
+
+        const bar = document.querySelector(`#day-${i+1} .bar`);
+        
+        // Animationg the height of the bar
+        const setHeight = async () => {
+            await delay(100);
+            bar.style.height=`${height}rem`;
+          };
+          setHeight(); 
       });
 }
 if(jsonFile) {
@@ -30,4 +40,5 @@ else {
     alert('Sorry! No data for you.');
 }
 
-});
+const today = document.querySelector('#day-3 .bar');
+today.classList.add('turquoise');
